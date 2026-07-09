c1970e7b24ad206890777ce8f64618629b47f874
Muhammad-Sheraz90 <116351736+Muhammad-Sheraz90@users.noreply.github.com>
2026-07-09 14:13:35 +0400

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -5,26 +5,9 @@ def ema(series, period):
 
 def check_signal(data):
 
-    if len(data) < 200:
-        return None
-
-    data["EMA50"] = ema(data["close"], 50)
-    data["EMA200"] = ema(data["close"], 200)
-
     last = data.iloc[-1]
 
-    if last["EMA50"] > last["EMA200"] and last["close"] > last["EMA200"]:
-
-        return {
-            "type": "BUY",
-            "entry": round(last["close"],2)
-        }
-
-    if last["EMA50"] < last["EMA200"] and last["close"] < last["EMA200"]:
-
-        return {
-            "type": "SELL",
-            "entry": round(last["close"],2)
-        }
-
-    return None
+    return {
+        "type": "BUY",
+        "entry": round(last["close"], 2)
+    }
