0f061c258c2df5ea96e5a0bb34496d15d4a82d48
Muhammad-Sheraz90 <116351736+Muhammad-Sheraz90@users.noreply.github.com>
2026-07-09 08:56:10 +0400

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -8,8 +8,8 @@ def check_signal(data):
     if len(data) < 200:
         return None
 
-    data["EMA50"] = ema(data["Close"], 50)
-    data["EMA200"] = ema(data["Close"], 200)
+    data["EMA50"] = ema(data["close"], 50)
+    data["EMA200"] = ema(data["close"], 200)
 
     last = data.iloc[-1]
 
@@ -17,14 +17,14 @@ def check_signal(data):
 
         return {
             "type": "BUY",
-            "entry": round(last["Close"],2)
+            "entry": round(last["close"],2)
         }
 
     if last["EMA50"] < last["EMA200"] and last["Close"] < last["EMA200"]:
 
         return {
             "type": "SELL",
-            "entry": round(last["Close"],2)
+            "entry": round(last["close"],2)
         }
 
     return None
