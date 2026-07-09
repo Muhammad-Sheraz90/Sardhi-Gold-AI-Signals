0ba7705e9e0ca160e2cbb933c8ccba9ff8f986df
Muhammad-Sheraz90 <116351736+Muhammad-Sheraz90@users.noreply.github.com>
2026-07-09 15:09:35 +0400

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -4,14 +4,21 @@ def ema(series, period):
     return series.ewm(span=period, adjust=False).mean()
 
 def rsi(series, period=14):
-    delta = series.diff()
+    delta = series.diff()   
+
 
     gain = delta.where(delta > 0, 0).rolling(period).mean()
     loss = (-delta.where(delta < 0, 0)).rolling(period).mean()
 
     rs = gain / loss
     return 100 - (100 / (1 + rs))
+def atr(data, period=14):
+    high_low = data["high"] - data["low"]
+    high_close = (data["high"] - data["close"].shift()).abs()
+    low_close = (data["low"] - data["close"].shift()).abs()
 
+    tr = pd.concat([high_low, high_close, low_close], axis=1).max(axis=1)
+    return tr.rolling(period).mean()
 
 def check_signal(data):
 
