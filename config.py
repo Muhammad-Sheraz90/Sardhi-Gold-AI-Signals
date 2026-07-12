506e9cb2543b2cf189d7ca63d3112f25cc5f3461
Muhammad-Sheraz90 <116351736+Muhammad-Sheraz90@users.noreply.github.com>
2026-07-12 08:19:40 +0400

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
+TWELVEDATA_API_KEY = os.getenv("TWELVEDATA_API_KEY")
 print("BOT_TOKEN:", BOT_TOKEN)
 print("CHAT_ID:", CHAT_ID)
 SYMBOL = "XAUUSD"
