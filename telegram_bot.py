8bf0a3e5eac347f61d9cdcebff40d124d057b3f2
Muhammad-Sheraz90 <116351736+Muhammad-Sheraz90@users.noreply.github.com>
2026-07-13 14:39:28 +0400

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,21 +2,19 @@ import requests
 from config import BOT_TOKEN, CHAT_ID
 
 def send_signal(message):
-
+    # ٹوکن کو صاف ستھرا لنک میں شامل کرنا
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
     payload = {
         "chat_id": CHAT_ID,
-        "text": message
+        "text": message,
+        "parse_mode": "Markdown"  # اس سے ڈیزائن خوبصورت دیکھے گا
     }
 
-    response = requests.post(url, data=payload)
-
-    print(response.status_code)
-    print(response.text)
-    print(response.status_code)
-    print(response.text)
-    print(response.json())
-    print("BOT_TOKEN =", BOT_TOKEN)
-    print("CHAT_ID =", CHAT_ID)
-    return response.status_code == 200
+    try:
+        response = requests.post(url, json=payload, timeout=10)
+        print(f"Telegram Server Response Code: {response.status_code}")
+        return response.status_code == 200
+    except Exception as e:
+        print(f"Telegram Send Error: {e}")
+        return False
