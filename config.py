430732d5446bd6639bb8c45b43dc91b584936ab9
Muhammad-Sheraz90 <116351736+Muhammad-Sheraz90@users.noreply.github.com>
2026-07-14 11:49:21 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,3 @@
-import os
-
-# --- ٹیلی گرام کنفیگریشن ---
-BOT_TOKEN = "8725455662:AAFbNrhovW6Mh_gT0JA9E2nFJxj2BCTUX-8"
+# اپنا ٹیلی گرام بوٹ ٹوکن اور چیٹ آئی ڈی یہاں لکھیں
+TELEGRAM_TOKEN = "8725455662:AAFbNrhovW6Mh_gT0JA9E2nFJxj2BCTUX-8"
 CHAT_ID = "7358356587"
-
-# --- مارکیٹ ڈیٹا کنفیگریشن ---
-SYMBOL = "GC=F"       # یاہو فنانس کے لیے گولڈ کا درست ٹکر
-TIMEFRAME = "5m"      # 5 منٹ کا ٹائم فریم
-
-print("Config loaded with secure direct endpoints.")
